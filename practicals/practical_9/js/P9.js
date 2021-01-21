4367a3decae7982e4fc33ac61cc32ad5e6dfb2d3
Chris Paul <chrispaul98@icloud.com>
2021-01-21 00:11:35 +0000

    1
    1

--- practicals/practical_9/js/P9.js
+++
@@ -1,5 +0,0 @@
-function getSelectValue(formname,selectname){
- var theMenu = document[formname][selectname];
- var selecteditem = theMenu.selectedIndex;
- return theMenu.options[selecteditem].value;
-}
